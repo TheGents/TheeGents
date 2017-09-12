5a99a6c5acfcb70ae6c2f36000c79999e49e855f
vubui237 <vtb0977@gmail.com>
2017-09-12 15:55:47 -0500

    Added auth0 login

--- App/components/Register/LoginButton/LoginButton.js
+++ App/components/Register/LoginButton/LoginButton.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
-import Button from 'react-native-button';
+import {
+  AppRegistry,
+  Text,
+  View,
+  Image,
+  Button
+} from 'react-native';
+
+import Auth0 from 'react-native-auth0';
+
+let credentials = require('../../Register/auth0-credentials.js');
+const auth0 = new Auth0(credentials);
 
 export default class LoginButton extends Component {
-    constructor(props, context) {
-        super(props, context);
-      }
-      _handlePress() {
-        console.log('Pressed!');
-      }
+constructor(props) {
+        super(props);
+        this.state = { accessToken: null };
+    }
+    _onLogin = () => {
+        auth0.webAuth
+          .authorize({
+            scope: 'openid profile',
+            audience: 'https://' + credentials.domain + '/userinfo'
+          })
+          .then(credentials => {
+            Alert.alert(
+              'Success',
+              'AccessToken: ' + credentials.accessToken,
+              [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+              { cancelable: false }
+            );
+            this.setState({ accessToken: credentials.accessToken });
+          })
+          .catch(error => console.log(error));
+      };
+    _onLogout = () => {
+    if (Platform.OS === 'android') {
+      this.setState({ accessToken: null });
+    } else {
+      auth0.webAuth
+        .clearSession({})
+        .then(success => {
+          this.setState({ accessToken: null });
+        })
+        .catch(error => console.log(error));
+    }
+  };
       render() {
+        let loggedIn = this.state.accessToken === null ? false : true;
         return (
           <Button
-            containerStyle={{height:30, overflow:'hidden', borderRadius:4, backgroundColor: 'black'}}
+            onPress={loggedIn ? this._onLogout : this._onLogin}
+            title={loggedIn ? 'Log Out' : 'Log In'}
             style={{fontSize: 20, color: 'white'}}
-            onPress={() => this._handlePress()}>
-            Login
-          </Button>
+            containerStyle={{height:30, overflow:'hidden', borderRadius:4, backgroundColor: 'black'}}
+             />
         );
       }
 }
